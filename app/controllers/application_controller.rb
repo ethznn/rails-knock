6ba8b09cfbd499b6f8d2866755ba04ac2a2e1ba4
prayeo <prxyeo@gmail.com>
2018-05-16 21:18:28 +0900

    return jwt $ curl --data auth[email]=%%%&auth[password]=%%% url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  include Knock::Authenticable
+
+  before_action :set_default_format
+
+  private
+  def :set_default_format
+    request.format = :json
+  end
 end
